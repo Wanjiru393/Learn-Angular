13b5c6b5535829f81d8d307b5f41779e16490204
Wanjiru393 <carolciru393@gmail.com>
2022-04-09 09:31:05 +0300

    Two-way Data-Binding Tackled

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,15 +8,17 @@ import { CarComponent } from './Intro-to-Angular/car-component/car.component';
 import { MyComponent } from './Intro-to-Angular/my-component/my.component';
 import { InterPolationComponent } from './Basics/Data-Binding/inter-polation/inter-polation.component';
 import { PropertyBindingComponent } from './Basics/Data-Binding/property-binding/property-binding.component';
-import { EventBindingComponent } from './Basics/Data-Binding/event-binding/event-binding.component'
+import { EventBindingComponent } from './Basics/Data-Binding/event-binding/event-binding.component';
+import { TwoWayDataBindingComponent } from './Basics/Data-Binding/two-way-data-binding/two-way-data-binding.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,CarComponent,MyComponent, InterPolationComponent, PropertyBindingComponent, EventBindingComponent,
+    AppComponent,CarComponent,MyComponent, InterPolationComponent, PropertyBindingComponent, EventBindingComponent, TwoWayDataBindingComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
